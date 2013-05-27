12b4765b1572aa434185097da72105f09c0e3902
MAYAH <mayah@mayah.jp>
2013-05-27 22:52:10 +0900

    GUI added

--- Reversi/src/main/java/Main.java
+++ Reversi/src/main/java/Main.java
@@ -20,8 +20,8 @@ import board.Turn;
 
 public class Main {
     public static void main(String[] args) {
-        //Player blackPlayer = new RandomPlayer(Turn.BLACK);
-        Player blackPlayer = new SimpleMonteCarloPlayer(Turn.BLACK, 100);
+        Player blackPlayer = new RandomPlayer(Turn.BLACK);
+        //Player blackPlayer = new SimpleMonteCarloPlayer(Turn.BLACK, 100);
         //Player blackPlayer = new UCB1MonteCarloPlayer(Turn.BLACK, 1000);
         //Player blackPlayer = new HumanPlayer(Turn.BLACK);
         //Player blackPlayer = new SimpleAI(Turn.BLACK);
@@ -29,10 +29,10 @@ public class Main {
         //Player blackPlayer = new AlphaBetaSimpleAI(Turn.BLACK, 5);
         //Player whitePlayer = new SimpleAI(Turn.WHITE);
         //Player whitePlayer = new SimpleMonteCarloPlayer(Turn.WHITE, 100);
-        Player whitePlayer = new UCB1MonteCarloPlayer(Turn.WHITE, 1000);
+        //Player whitePlayer = new UCB1MonteCarloPlayer(Turn.WHITE, 1000);
         //Player whitePlayer = new MinMaxSimpleAI(Turn.WHITE, 5);
         //Player whitePlayer = new NegaMaxSimpleAI(Turn.WHITE, 5);
-        //Player whitePlayer = new AlphaBetaSimpleAI(Turn.WHITE, 5);
+        Player whitePlayer = new AlphaBetaSimpleAI(Turn.WHITE, 5);
         //Player whitePlayer = new AlphaBetaEvaluationSimpleWithCompleteReadingAI(Turn.WHITE, 5, 15);
         //Player whitePlayer = new NegaScoutEvaluationSimpleAI(Turn.WHITE, 5);
         //Player whitePlayer = new NegaScoutEvaluationSimpleWithCompleteReadingAI(Turn.WHITE, 5, 15);
@@ -43,10 +43,10 @@ public class Main {
         Board board = new Board();
         board.setup();
         Winner winner = game.play(blackPlayer, whitePlayer, board, Turn.BLACK, true);
-        
+
         System.out.println("BLACK: " + blackPlayer.getClass().getSimpleName());
         System.out.println("WHITE: " + whitePlayer.getClass().getSimpleName());
         System.out.println("Winner: " + winner);
     }
-    
+
 }
